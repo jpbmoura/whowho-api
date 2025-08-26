2b090d6e92d5fa8866a7191e6f16bc0a8e735a1c
João Pedro <jopmoura49@gmail.com>
2025-08-26 10:38:48 -0300

    try to fix deploy error

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { webcrypto } from 'crypto';
+
+// Polyfill for crypto.randomUUID() if not available globally
+if (!global.crypto) {
+  global.crypto = webcrypto as any;
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
